74848076fe4c4feb68afb7e7d1c3f628cc560bc3
FILÂTRE Killian <k.filatre@gmail.com>
2024-08-24 00:26:46 +0200

    done square root

---
+++ terre/terre10.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Returns the square root of the number entered as argument
+ *
+ * Prohibited function : sqrt()
+ */
+
+if ($argc = 1) {
+
+    $argument = $argv[1];
+
+    if (ctype_digit($argument) && $argument >= '0') {
+
+        $result = 1;
+        while ($argument > $result) {
+            $argument = ($argument + $result) / 2;
+            $result = $argv[1] / $argument;
+        }
+
+        echo("Result: " . $result . PHP_EOL);
+
+        return;
+    }
+
+    echo('Your calculation is strange' . PHP_EOL);
+
+    return;
+}
+
+echo ('You must only enter one number in your arguments' . PHP_EOL);
+
+// run in terminal => php terre05.php 9
+// Result : 3
