6d92e8fbe56824b5c31cc82266c283be50daec77
FILÂTRE Killian <k.filatre@gmail.com>
2024-08-23 12:06:12 +0200

    done display arguments

--- terre/terre02.php
+++ terre/terre02.php
@@ -1,6 +1,13 @@
 <?php
+
+/**
+ * Represents the filename of a file.
+ *
+ * @var string $filename The filename of the file.
+ */
+
 $filename = $_SERVER['SCRIPT_NAME'];
 
 echo ($filename);
 
-// run in terminale php terre02.php
+// run in terminal => php terre02.php
---
+++ terre/terre03.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Represents the argument passed to a function or method.
+ *
+ * @var mixed $myArgument The value of the argument.
+ */
+
+if ($argc > 1) {
+
+    $myArgument = $argv[1];
+
+    $myArguments = explode(' ', $myArgument);
+
+    foreach ($myArguments as $index => $argument) {
+        echo($argument . PHP_EOL);
+    }
+
+    return;
+}
+
+$myArguments = 'You have not entered any arguments';
+
+echo ($myArguments);
+
+
+// run in terminal => php terre03.php "Hello my name is Killian"
